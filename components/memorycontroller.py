f0df2009efecf7692976ea6bdd3389075c6390c2
wcafricanus <wcafricanus@gmail.com>
2019-11-20 11:50:52 +0800

    memory controller simplified according to Trevor's comments

--- components/memorycontroller.py
+++ components/memorycontroller.py
@@ -1,45 +1,21 @@
 class MemoryController:
     def __init__(self):
-        self.counter = 0
         self.loading = {}
-        self.loading_listeners = {}
         self.storing = {}
-        self.storing_listerners = {}
 
     def fetch_block(self, requester, address):
-        listeners = self.loading_listeners.setdefault(address, [])
-        if listeners:
-            raise PermissionError("cache {} is already fetching this address from memory".format(listeners[0].name))
-        listeners.append(requester)
-        self.loading[address] = 100
+        self.loading[requester] = 100
 
     def evict_block(self, requester, address):
-        self.storing[address] = 100
-        storing_listeners = self.storing_listerners.setdefault(address, [])
-        storing_listeners.append(requester)
+        self.storing[requester] = 100
 
     def tick(self):
-        self.counter += 1
-        finished = []
-        for address in self.loading:
-            self.loading[address] -= 1
-            if self.loading[address] == 0:
-                finished.append(address)
+        for requester in self.loading:
+            self.loading[requester] -= 1
+            if self.loading[requester] < 1:
+                requester.on_fetch_from_memory_finished()
 
-        for address in finished:
-            self.loading.pop(address)
-            for listener in self.loading_listeners[address]:
-                listener.on_fetch_from_memory_finished()
-            self.loading_listeners.pop(address)
-
-        finished = []
-        for address in self.storing:
-            self.storing[address] -= 1
-            if self.storing[address] == 0:
-                finished.append(address)
-
-        for address in finished:
-            self.storing.pop(address)
-            for listener in self.storing_listerners[address]:
-                listener.on_evict_to_memory_finished()
-            self.storing_listerners.pop(address)
+        for requester in self.storing:
+            self.storing[requester] -= 1
+            if self.storing[requester] < 1:
+                requester.on_evict_to_memory_finished()
--- test/test_memory.py
+++ test/test_memory.py
@@ -5,32 +5,100 @@ class SimpleRequester:
     def __init__(self, name, mc):
         self.mc = mc
         self.name = name
+        self.fetched_block = None
+        self.evicted_block = None
 
     def on_fetch_from_memory_finished(self):
-        print("[{}]{} fetch from memory finished".format(self.mc.counter, self.name))
+        self.fetched_block = 1
 
     def on_evict_to_memory_finished(self):
-        print("[{}]{} evict to memory finished".format(self.mc.counter, self.name))
+        self.evicted_block = 1
+
+    def reset(self):
+        self.fetched_block = None
+        self.evicted_block = None
 
 
-def test_memory_controller():
+def test_fetch_block_from_memory():
     mc = MemoryController()
     r0 = SimpleRequester('P0', mc)
     r1 = SimpleRequester('P1', mc)
     r2 = SimpleRequester('P2', mc)
-    r3 = SimpleRequester('P3', mc)
     mc.fetch_block(r0, 0b1)
     for _ in range(99):
         mc.tick()
+    assert r0.fetched_block is None
+    mc.tick()
+    assert r0.fetched_block is not None
+    r0.reset()
 
-    mc.evict_block(r1, 0b10)
-    for _ in range(40):
+    mc.fetch_block(r0, 0b1)
+    for _ in range(50):
         mc.tick()
+    assert r0.fetched_block is None
+    mc.fetch_block(r1, 0b1)
+    mc.fetch_block(r2, 0b11)
+    for _ in range(50):
+        mc.tick()
+    assert r0.fetched_block is not None
+    assert r1.fetched_block is None
+    assert r2.fetched_block is None
+    for _ in range(50):
+        mc.tick()
+    assert r1.fetched_block is not None
+    assert r2.fetched_block is not None
 
-    mc.evict_block(r2, 0b10)
-    for _ in range(100):
-        mc.tick()
 
-    mc.fetch_block(r0, 0b11)
-    for _ in range(100):
+def test_evict_block_to_memory():
+    mc = MemoryController()
+    r0 = SimpleRequester('P0', mc)
+    r1 = SimpleRequester('P1', mc)
+    r2 = SimpleRequester('P2', mc)
+    mc.evict_block(r0, 0b1)
+    for _ in range(99):
         mc.tick()
+    assert r0.evicted_block is None
+    mc.tick()
+    assert r0.evicted_block is not None
+    r0.reset()
+
+    mc.evict_block(r0, 0b1)
+    for _ in range(50):
+        mc.tick()
+    assert r0.evicted_block is None
+    mc.evict_block(r1, 0b1)
+    mc.evict_block(r2, 0b11)
+    for _ in range(50):
+        mc.tick()
+    assert r0.evicted_block is not None
+    assert r1.evicted_block is None
+    assert r2.evicted_block is None
+    for _ in range(50):
+        mc.tick()
+    assert r1.evicted_block is not None
+    assert r2.evicted_block is not None
+
+
+def test_interleaving_evict_and_fetch_memory():
+    mc = MemoryController()
+    r0 = SimpleRequester('P0', mc)
+    r1 = SimpleRequester('P1', mc)
+    r2 = SimpleRequester('P2', mc)
+    mc.fetch_block(r0, 0b1)
+    for _ in range(50):
+        mc.tick()
+    assert r0.fetched_block is None
+    mc.evict_block(r0, 0b10)
+    mc.evict_block(r1, 0b1)
+    mc.evict_block(r2, 0b11)
+    for _ in range(50):
+        mc.tick()
+    assert r0.fetched_block is not None
+    assert r0.evicted_block is None
+    assert r1.evicted_block is None
+    assert r2.evicted_block is None
+    for _ in range(50):
+        mc.tick()
+    assert r0.evicted_block is not None
+    assert r1.evicted_block is not None
+    assert r2.evicted_block is not None
